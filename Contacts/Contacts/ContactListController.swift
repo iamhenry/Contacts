f3c26dc3bc1272c04288aeabc385bb6fc1157599
iamhenry <virtualhenry@gmail.com>
2017-12-27 16:59:11 -0800

    Added cells with data to display

---
+++ Contacts/Contacts/ContactListController.swift
@@ -0,0 +1,48 @@
+//
+//  ContactListController.swift
+//  Contacts
+//
+//  Created by  Henry Moran on 12/27/17.
+//  Copyright © 2017 Henry Moran. All rights reserved.
+//
+
+import UIKit
+
+class ContactListController: UITableViewController {
+    
+    var contacts = ContactsSource.contacts
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        
+        let contact = contacts[indexPath.row]
+        
+        cell.textLabel?.text = contact.firstName
+        
+        return cell
+    }
+
+
+
+
+
+}
